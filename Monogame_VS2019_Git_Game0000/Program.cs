b8ba13f66d70259ffbd75c5f3ee59cdb406d01ae
SameplayerDE <asameplayer@gmail.com>
2020-11-27 17:35:46 +0100

    init

---
+++ Monogame_VS2019_Git_Game0000/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Monogame_VS2019_Git_Game0000
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
